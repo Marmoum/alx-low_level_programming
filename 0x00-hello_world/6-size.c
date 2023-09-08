804db3d8818d53ce85c598554d6bc6b0db1e1119
Marmoum <marouanemoumanis@gmail.com>
2023-09-08 03:51:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,19 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
+#ifdef __x86_64__
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", sizeof(li));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 printf("Size of a float:%lu byte(s)\n", sizeof(f));
+#else
+printf("Size of a char: %u byte(s)\n", sizeof(c));
+printf("Size of an int: %u byte(s)\n", sizeof(i));
+printf("Size of a long int: %u byte(s)\n", sizeof(li));
+printf("Size of a long long int: %u byte(s)\n", sizeof(lli));
+printf("Size of a float:%u byte(s)\n", sizeof(f));
+#endif
 return (0);
 }
