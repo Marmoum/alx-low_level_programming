01b16b0a8792ef9c9792ea161643253b4ee85c23
Marmoum <marouanemoumanis@gmail.com>
2023-09-08 02:30:45 -0700

    alx-low_level_programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  * main - Entry of the program
  * Return: Always returns (0)
  */
-int main(void) {
+int main(void) 
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
 }
