e505faf799278e0dfa8e23d8692fc23f28709a6f
Marmoum <marouanemoumanis@gmail.com>
2023-09-11 17:16:26 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,14 @@
  * Return: always 0
  */
 
-int main (void)
+int main(void)
 {
 	char i;
+
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
-                putchar(i);
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
