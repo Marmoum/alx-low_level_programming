52e8b1626eb40eedaaebcebd571aad99f4d9785f
Marmoum <marouanemoumanis@gmail.com>
2023-09-13 16:31:15 -0700

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main func
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char b = 'a';
+
+	while (b <= 'z')
+	{
+		_putchar(b);
+		b++;
+
+	}
+	_putchar('\n');
+
+}
