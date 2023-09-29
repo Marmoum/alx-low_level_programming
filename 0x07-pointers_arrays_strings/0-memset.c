7c29c8731ccd9d9b7cf450d2ca49df37135ffe76
Marmoum <marouanemoumanis@gmail.com>
2023-09-28 19:23:38 -0700

    chi la3ba

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
-	for (; n > 0; i++)
+	unsigned int i = 0;
+
+	while (i < n)
 	{
 		s[i] = b;
-		n--;
+		i++;
 	}
-		return (s);
+	return (s);
 }
