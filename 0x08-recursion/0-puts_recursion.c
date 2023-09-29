f6db4da52d82f0394b653ac61c671abe4b0aa4e4
Marmoum <marouanemoumanis@gmail.com>
2023-09-28 18:34:56 -0700

    betty dzb

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * _puts_recursion is the same as puts
+ * _puts_recursion - function like puts();
  * @s: input
- * Return: always 0
+ * Return: nothing just printing
  */
 
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
-		 _putchar(*s);
-		_puts_recursion(s+1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
 	else
-	_putchar('\n');
+		_putchar('\n');
 }
+
